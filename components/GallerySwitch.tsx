aca9c59f18e17af235ad5de6fcc91ca003c0bae6
Kian Malakooti <kianjmalakooti@gmail.com>
2024-09-16 18:04:42 -0700

    feat: components

--- components/GallerySwitch.tsx
+++ components/GallerySwitch.tsx
@@ -1,3 +1,41 @@
-export default function GallerySwitch() {
-  return <div className="w-screen h-screen">GallerySwitch</div>;
+import { imageGalleries } from "@/utils/types";
+import { useRouter } from "next/navigation";
+
+export default function Page() {
+  const router = useRouter();
+
+  const handleAlbumClick = (id: string) => {
+    router.push(`/album/${id}`);
+  };
+
+  return (
+    <div className="mt-24 flex-1 flex justify-center items-center p-4 md:p-12">
+      <div className="min-h-full w-full max-w-6xl space-y-4">
+        <span className="text-6xl text-black">Albums</span>
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          {imageGalleries.map((gallery) => (
+            <div
+              key={gallery.id}
+              className="relative flex flex-col items-center justify-center text-black text-center cursor-pointer"
+              onClick={() => handleAlbumClick(gallery.id)}
+            >
+              <div className="relative w-full h-[300px] overflow-hidden rounded-md">
+                <img
+                  src={gallery.logoSrc}
+                  className="w-full h-full object-cover"
+                  alt="Descriptive Alt Text"
+                />
+                <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-300 rounded-md">
+                  <span className="text-white text-xl p-4">
+                    {gallery.description}
+                  </span>
+                </div>
+              </div>
+              <span className="text-xl mt-2">{gallery.albumName}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ components/MasonryGrid.tsx
@@ -0,0 +1,18 @@
+import { generateImagePaths } from "@/utils/generateImagePath";
+import { AlbumProps } from "@/utils/types";
+
+export default function MasonryGrid({ album }: { album: AlbumProps }) {
+  const images = generateImagePaths(
+    album.directory,
+    album.fileName,
+    album.count
+  );
+
+  return (
+    <div className="columns-1 sm:columns-2 lg:columns-3 px-12 md:px-24 lg:px-36 ">
+      {images.map((image, index) => (
+        <img src={image} key={index} className="rounded-lg mb-4" />
+      ))}
+    </div>
+  );
+}
